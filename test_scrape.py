3fc1a7fa627ad643ef843638a24b825122c2aeb2
Mattias Ehatamm <mati@ehatamm.com>
2024-11-15 11:18:57 -0500

    parse sqbs

--- test_scrape.py
+++ test_scrape.py
@@ -4,13 +4,63 @@ import re
 domain = "https://hsquizbowl.org/db/"
 url = "https://hsquizbowl.org/db/tournaments/9209/stats/round_robin_%281-11%29/rounds/"
 
+tournaments = [["Penn Bowl 2024", "https://hsquizbowl.org/db/tournaments/9209/stats/round_robin_%281-11%29/games/#round-1"],
+               ["Penn Bowl 2024", "https://hsquizbowl.org/db/tournaments/9209/stats/finals/games/#round-12"],
+               ["PLAYTIME!", "https://hsquizbowl.org/db/tournaments/9116/stats/combined/games/#round-1"],
+               ["ACF Fall 2024", "https://hsquizbowl.org/db/tournaments/9045/stats/fixed_combined/games/#round-1"],
+               ["NAQT Novice Toronto 2024", "https://hsquizbowl.org/db/tournaments/9057/stats/all_games_%28raw%29/games/#round-1"],
+               ["NAQT Novice McGill 2024", "https://hsquizbowl.org/db/tournaments/9073/stats/all_games/games/#round-1"],
+               ["CREEK", "https://hsquizbowl.org/db/tournaments/8843/stats/all_games/games/#round-1"],
+               ["ESPN", "https://hsquizbowl.org/db/tournaments/8753/stats/prelims_%2B_playoffs/games/#round-1"],
+               ["Booster Shot", "https://hsquizbowl.org/db/tournaments/8758/stats/combined%2Bfinals/games/#round-1"],
+               ["SCT D2 2024", "https://hsquizbowl.org/db/tournaments/8703/stats/d2_all/games/#round-1"],
+               ["SCT D1 2024", "https://hsquizbowl.org/db/tournaments/8703/stats/d1/games/#round-1"],
+               ["ACF Regionals 2024", "https://hsquizbowl.org/db/tournaments/8698/stats/prelims/games/#round-1"],
+               ["ACF Regionals 2024", "https://hsquizbowl.org/db/tournaments/8698/stats/finals/rounds/"],
+               ["ILLIAC", "https://hsquizbowl.org/db/tournaments/8679/stats/all_games/games/#round-1"],
+               ["ACF Winter 2023", "https://hsquizbowl.org/db/tournaments/8536/stats/combined/games/#round-1"],
+               ["ACF Fall 2023", "https://hsquizbowl.org/db/tournaments/8328/stats/playoffs/games/#round-1"],
+               ["Penn Bowl 2023", "https://hsquizbowl.org/db/tournaments/8397/stats/combined/games/#round-1"],
+               ["DMA", "https://hsquizbowl.org/db/tournaments/8423/stats/combined/games/#round-1"],
+               ["Arcadia 2023", "https://hsquizbowl.org/db/tournaments/8529/stats/all_games/games/#round-1"],
+               ["C++", "https://hsquizbowl.org/db/tournaments/7944/stats/combined/games/#round-1"],
+               ["ACF Regionals 2023", "https://hsquizbowl.org/db/tournaments/7982/stats/combined/games/#round-1"],
+               ["SCT D2 2023", "https://hsquizbowl.org/db/tournaments/7863/stats/d2_all/games/#round-1"],
+               ["SCT D1 2023", ]]
 
-
-def get_games_from_round(round_url):
-    response = requests.get(round_url)
+def parse_team_line(team_line):
+    team_name, players = team_line.split(': ')
+    players = players.split(', ')
+    team = [team_name]
+    for player in players:
+        name = player[:player.find('(')-1]
+        player = player[player.find(')')+2:]
+        scores = [int(word) for word in player.split()[:3]]
+        team.append([[name]+scores])
+    return team
+def get_games_from_rounds_sqbs(scoreboard_url):
+    response = requests.get(scoreboard_url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+    else:
+        raise ValueError("Website {scoreboard_url} not accessible")
+    #print(soup.prettify())
+    games = soup.find_all('font', {'size': '-1'})
+    game_reports = []
+    for game in games:
+        stripped_lines = [line.strip() for line in game.stripped_strings]
+        team_a = parse_team_line(stripped_lines[0])
+        team_b = parse_team_line(stripped_lines[1])
+        bpts_a, bpts_b = [int(word) for word in stripped_lines[2].split() if word.isdigit()][1::2]
+        game_reports.append([team_a, bpts_a, team_b, bpts_b])
+    return game_reports
+def get_games_from_rounds(rounds_url):
+    response = requests.get(rounds_url)
     if response.status_code == 200:
         soup = BeautifulSoup(response.content, 'html.parser')
         games = soup.find_all('table')[3:]
+    else:
+        raise ValueError("Website {rounds_url} not accessible")
     game_csvs=[]
     for table in games:
         rows = table.find_all('tr')
@@ -44,5 +94,10 @@ def get_games_from_round(round_url):
         game_csvs.append([team_a, team_b, score_a, score_b, team_a_players, team_b_players])
     return(game_csvs)    
 
-playtime_url = "https://hsquizbowl.org/db/tournaments/9116/stats/combined/games/#round-3"
-games = get_games_from_round(playtime_url)
+#playtime_url = "https://hsquizbowl.org/db/tournaments/9116/stats/combined/games/#round-3"
+#games = get_games_from_rounds(playtime_url)
+
+sct_url = "https://hsquizbowl.org/db/tournaments/7863/stats/d1_finals/games/"
+games = get_games_from_rounds_sqbs(sct_url)
+for game in games:
+     print(game)
